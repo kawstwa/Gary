0f9502ac4015ee0275c457e504f9e246d179bf55
Tural Abbasov <twa001@gmail.com>
2025-06-10 00:33:54 -0400

    update button keyboard focus

--- button.py
+++ button.py
@@ -1,11 +1,13 @@
 from PySide6.QtWidgets import QPushButton, QWidget
 from PySide6.QtCore import Qt
-
+from PySide6.QtGui import QKeyEvent
 
 class CreateButton(QPushButton):
     def __init__(self, text):
         super().__init__()
+        self.setAttribute(Qt.WidgetAttribute.WA_Hover)
         self.hasMouseTracking()
+        # self.hasTabletTracking()
         self.setText(text)
         self.setFixedSize(230,50)
         self.setStyleSheet("""
@@ -22,6 +24,10 @@ class CreateButton(QPushButton):
             }
             QPushButton:hover {
                 background-color: #0e5c8f;
+            }
+            QPushButton:focus {
+                background-color: #a0c4ff;
+                border: 2px solid #1d3557;
             }
             """)
         self.setAutoDefault(True)
